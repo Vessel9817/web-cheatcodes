f50288b9a86e32cf305b9c7ee5b10a07cc5efc1c
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2025-09-30 22:13:57 -0400

    Moved inline JS and CSS to external files
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

---
+++ demos/timed-button.js
@@ -0,0 +1,20 @@
+const but = /** @type {HTMLButtonElement} */ (document.querySelector('button'));
+let secs = 120;
+let step = () => {
+    but.innerText = `Please wait ${secs} second${secs === 1 ? '' : 's'}...`;
+    secs--;
+
+    if (secs <= 0) {
+        window.clearInterval(intervalId);
+
+        but.disabled = false;
+        but.innerText = 'Click to download';
+    }
+};
+
+const intervalId = window.setInterval(step, 1000);
+
+but.addEventListener('click', (e) => {
+    e.preventDefault();
+    alert('You clicked the button, you win the game!')
+});
