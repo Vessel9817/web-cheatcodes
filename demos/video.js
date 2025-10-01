fd8adf5765de2ab28989df166d775ee91fe651ae
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2025-09-30 22:18:20 -0400

    Separated file into HTML, CSS and JS files
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

---
+++ demos/video.js
@@ -0,0 +1,19 @@
+if (window.matchMedia('(pointer: fine)').matches) {
+    console.log('x');
+
+    let but = /** @type {HTMLButtonElement} */ (document.querySelector('button'));
+    let container = /** @type {HTMLElement} */ (document.getElementById('movie'));
+    let video = /** @type {HTMLVideoElement} */ (document.querySelector('video'));
+
+    video.controls = false;
+    but.addEventListener('click', () => {
+        container.classList.toggle('play');
+
+        if (container.classList.contains('play')) {
+            video.play();
+        }
+        else {
+            video.pause();
+        }
+    });
+}
